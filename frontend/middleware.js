db23f974b336607f6a9b0a9edcb600e6bba36b7b
Yash <yashdesh095@gmail.com>
2025-06-23 20:41:05 +0530

    fix: changed middleware file for deployment issue

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,23 +1,25 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/host-dashboard(.*)',
-  '/user(.*)',
-  '/listing/create-new',
-  '/listing/edit(.*)',
-])
+  "/host-dashboard(.*)",
+  "/user(.*)",
+  "/listing/create-new",
+  "/listing/edit(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth().protect()
+    await auth().protect({
+      unauthenticatedUrl: "/sign-in", // redirect if not logged in
+    });
   }
-})
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Skip Next.js internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
