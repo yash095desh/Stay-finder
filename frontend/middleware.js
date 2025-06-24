e9ad1ee8d9fb2d19c815295f1ce9c00f37b4f01b
Yash <yashdesh095@gmail.com>
2025-06-24 11:40:36 +0530

    fixing middleware file

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -11,7 +11,7 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth().protect()
+    await auth.protect()
   }
 })
 
